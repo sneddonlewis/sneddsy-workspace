1e98b04a1188378f032ddf86e210be0121eb765d
lewis sneddon <lewis@pop-os.localdomain>
2024-04-13 19:54:40 +0100

    krato-api: Simplified application project as having build problems referencing it

---
+++ apps/krato-api/krato-api-application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,8 @@
+using System.Threading.Tasks;
+
+namespace SneddsyWorkspace.Apps.KratoApi.KratoApiApplication.Contracts.Persistence;
+
+public interface IAsyncRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(Guid id);
+}
