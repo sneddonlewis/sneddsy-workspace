eb0733e4f1d7bdfcaae302c430839f65bf78df9e
lewis sneddon <lewis@pop-os.localdomain>
2024-04-13 22:53:14 +0100

    krato-api: Corrected IExerciseRepository type, interface

--- apps/krato-api/krato-api-application/Contracts/Persistence/IExerciseRepository.cs
+++ apps/krato-api/krato-api-application/Contracts/Persistence/IExerciseRepository.cs
@@ -2,7 +2,7 @@ using SneddsyWorkspace.Apps.KratoApi.KratoApiDomain.Entities;
 
 namespace SneddsyWorkspace.Apps.KratoApi.KratoApiApplication.Contracts.Persistence;
 
-public class IExerciseRepository : IAsyncRepository<Exercise> {
+public interface IExerciseRepository : IAsyncRepository<Exercise> {
   public Task<Exercise> GetByIdAsync(Guid id)
   {
     throw new NotImplementedException();
