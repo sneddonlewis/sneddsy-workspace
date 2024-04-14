9ce9a3053b414b45d9ebd2ae30a6232cdce738b6
lewis sneddon <lewis@pop-os.localdomain>
2024-04-14 14:54:20 +0100

    krato-api: Added Persistence project and packages for entity framework and sqlite

--- apps/krato-api/krato-api-persistence-test/UnitTest1.cs
+++ apps/krato-api/krato-api-persistence-test/UnitTest1.cs
@@ -1,15 +1,10 @@
 namespace SneddsyWorkspace.Apps.KratoApi.KratoApiPersistence.Test;
 
-public class Tests
+public class UnitTest1
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
+    [Fact]
     public void Test1()
     {
-        Assert.Pass();
+
     }
 }
