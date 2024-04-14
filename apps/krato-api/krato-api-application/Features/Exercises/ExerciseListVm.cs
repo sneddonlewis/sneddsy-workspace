d8462ab62c1b688287e60e03f0acb60804f41542
lewis sneddon <lewis@pop-os.localdomain>
2024-04-14 16:04:24 +0100

    krato-api: Exercise list vm returns id

--- apps/krato-api/krato-api-application/Features/Exercises/ExerciseListVm.cs
+++ apps/krato-api/krato-api-application/Features/Exercises/ExerciseListVm.cs
@@ -3,5 +3,6 @@ namespace SneddsyWorkspace.Apps.KratoApi.KratoApiApplication.Features.Exercises;
 
 public class ExerciseListVm
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
 }
