4e8d8cf8bfc5967a6a6fb36876d6dcd98d207e02
lewis sneddon <lewis@pop-os.localdomain>
2024-04-14 15:32:55 +0100

    krato-api: Getting ready to connect api endpoint

--- apps/krato-api/krato-api-application/Features/Exercises/GetExercisesListQueryHandler.cs
+++ apps/krato-api/krato-api-application/Features/Exercises/GetExercisesListQueryHandler.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using MediatR;
 using SneddsyWorkspace.Apps.KratoApi.KratoApiApplication.Contracts.Persistence;
+using SneddsyWorkspace.Apps.KratoApi.KratoApiDomain.Entities;
 
 namespace SneddsyWorkspace.Apps.KratoApi.KratoApiApplication.Features.Exercises;
 
@@ -20,7 +21,16 @@ public class GetExercisesListQueryHandler : IRequestHandler<GetExercisesListQuer
 
     public async Task<List<ExerciseListVm>> Handle(GetExercisesListQuery request, CancellationToken cancellationToken)
     {
-        var allExercises = await _exerciseRepo.ListAllAsync();
+        // var allExercises = await _exerciseRepo.ListAllAsync();
+        // TODO swap this for line above after runnig migration
+        var allExercises = new List<Exercise>
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Push Ups"
+            }
+        };
         return _mapper.Map<List<ExerciseListVm>>(allExercises);
     }
 }
