1d181cd582a2027a0cc42c2cff482ed4d5c39d68
lewis sneddon <lewis@pop-os.localdomain>
2024-04-14 09:25:34 +0100

    krato-api: Corrected IExerciseRepository initial definition

--- apps/krato-api/krato-api-application/Contracts/Persistence/IAsyncRepository.cs
+++ apps/krato-api/krato-api-application/Contracts/Persistence/IAsyncRepository.cs
@@ -1,5 +1,3 @@
-using System.Threading.Tasks;
-
 namespace SneddsyWorkspace.Apps.KratoApi.KratoApiApplication.Contracts.Persistence;
 
 public interface IAsyncRepository<T> where T : class
--- apps/krato-api/krato-api-application/Contracts/Persistence/IExerciseRepository.cs
+++ apps/krato-api/krato-api-application/Contracts/Persistence/IExerciseRepository.cs
@@ -2,9 +2,6 @@ using SneddsyWorkspace.Apps.KratoApi.KratoApiDomain.Entities;
 
 namespace SneddsyWorkspace.Apps.KratoApi.KratoApiApplication.Contracts.Persistence;
 
-public interface IExerciseRepository : IAsyncRepository<Exercise> {
-  public Task<Exercise> GetByIdAsync(Guid id)
-  {
-    throw new NotImplementedException();
-  }
+public interface IExerciseRepository : IAsyncRepository<Exercise>
+{
 }
